754cec2d783d767c734dc6c598fb579ffedd6dca
AAOOII-RN <aaooii.rn@gmail.com>
2025-08-24 21:37:02 +0800

    Rotating gradient

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ The main.lua file **creates a simple vertical linear gradient effect** using rec
 * Divides the window into horizontal strips 
 * gradually increases the opacity of each strip from top to bottom, resulting in a smooth gradient.
 
+# 08/24/25 (mm/dd/yy)
+* Can now rotate using [canvas](https://love2d.org/wiki/Canvas)
+
 ###### Credits to Github Copilot (I'm too lazy rn.)
--- main.lua
+++ main.lua
@@ -1,11 +1,24 @@
+local rects = 128
+local ww, wh = love.graphics.getDimensions()
+
 function love.load()
-    ww, wh = love.graphics.getDimensions()
+    gradient = love.graphics.newCanvas(1000, 1000)
+
+    love.graphics.setCanvas(gradient)
+    for i = 0, rects do
+        love.graphics.setColor(i/rects,0,0.5)
+        love.graphics.rectangle("fill", 0, i*(1000/rects), 1000, 1000/rects)
+    end
+    love.graphics.setCanvas()
+end
+
+function love.resize(w, h)
+    ww = w
+    wh = h
 end
 
 function love.draw()
-    local size = 64
-    for i = 0, wh/size do
-        love.graphics.setColor(1,1,1,i/(wh/size))
-        love.graphics.rectangle("fill", 0, i*size, ww, size)
-    end
+    love.graphics.setColor(1,1,1)
+    local scale = ((ww*ww + wh*wh)^0.5)/1000 -- I pay attention to my trigo classes
+    love.graphics.draw(gradient, ww/2, wh/2, os.clock(), scale, scale, gradient:getWidth()/2, gradient:getHeight()/2)
 end
